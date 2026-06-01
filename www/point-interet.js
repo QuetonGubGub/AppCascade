a4acc22f0395a95ca5f4e7fdd8ba09f105164435
GalaxyDuFeu22 <grouxoceane@gmail.com>
2026-06-01 16:59:22 +0200

    Ajout des points d'intérêt au complet (sans vidéos)

---
+++ www/point-interet.js
@@ -0,0 +1,28 @@
+fetch("point-interet.json")
+.then(res => res.json())
+.then(data => {
+
+    const params = new URLSearchParams(window.location.search);
+    const nomPoint = params.get("nom");
+
+    const nom = data[nomPoint];
+
+    document.getElementById("nom-interet").textContent = nom["nom"];
+    document.getElementById("image-interet").src = nom["image"];
+    document.getElementById("description-interet").textContent = nom["description-1"];
+    document.getElementById("description-interet").textContent += nom["description-2"];
+    document.getElementById("description-interet").textContent += nom["description-3"];
+    document.getElementById("prochain-point").textContent = nom["prochain"];
+    document.getElementById("lien-video").href = nom["video"];
+
+    /*Cacher le bouton vidéo si src = "#"*/
+    const lienVideo = document.getElementById("plus");
+    const video = nom["video"];
+
+    if (video === "#") {
+        lienVideo.style.display = "none";
+    } else {
+        lienVideo.href = video;
+        lienVideo.style.display = "";
+    }
+});
