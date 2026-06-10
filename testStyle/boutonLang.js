14db801bfa352355bd6c147a5b692be84e115e53
QuetonGubGub <qp.compteprive95@gmail.com>
2026-06-10 08:54:12 +0200

    Création du bouton des langues

---
+++ testStyle/boutonLang.js
@@ -0,0 +1,33 @@
+// Changement de la langue
+function changeLang(l = "fr") {
+
+}
+
+// Activation du bouton
+function showLang(actif = false) {
+    console.log("Touché")
+
+    // On récupère les informations du bouton
+    document.getElementById("btnLang")
+
+    // On change le bouton
+    if (!actif) {
+        console.log("Bouton activé")
+        // On crée le menu
+        // On ajoute son css
+        // On ajoute les sous-boutons
+        // On ajoute leur classe et leur onlick
+        // On réecrit le css du bouton
+        // On réecrit aussi son onlick
+
+    } else {
+        console.log("Bouton désactivé")
+        // On cherche le menu
+        // On vide son inner html
+        // On réecrit le css du bouton
+        // On réecrit aussi son onlick
+
+    }
+}
+
+console.log("Script importé")
